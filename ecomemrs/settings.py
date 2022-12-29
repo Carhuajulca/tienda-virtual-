5c220b22107ea63463eb1f3270ba73b27398e7c2
Gesler <carhuajulca@outlook.es>
2022-12-29 17:52:16 -0500

    configutacion de archivos media

--- ecomemrs/settings.py
+++ ecomemrs/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from pathlib import Path, PurePath
+MEDIA_ROOT = PurePath(BASE_DIR, 'media')
+MEDIA_URL ='media/'
