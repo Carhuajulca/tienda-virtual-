d2a893e4e82777120550e3c6ecd2f6ec7dda27e2
Gesler <carhuajulca@outlook.es>
2022-12-30 17:56:47 -0500

    Ceacion de los modelos

--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from producto.models import *
 
 # Register your models here.
+admin.site.register(Marca)
+admin.site.register(Color)
+admin.site.register(Medida)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Presentacion)
+admin.site.register(Imagen)
--- producto/models.py
+++ producto/models.py
@@ -2,13 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Marca(models.Model):
-    nobre = models.CharField("Nombre", max_length=50)
+    nombre = models.CharField("Nombre", max_length=50)
+    
+    def __str__(self):
+        return self.nombre
 
 class Color(models.Model):
     nombre = models.CharField("Nombre", max_length=50)
+   
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Color'
+        verbose_name_plural = "Colores"   
+
 
 class Medida(models.Model):
     nombre = models.CharField("Nombre", max_length=50)
+
+    def __str__(self):
+       return self.nombre 
+
 
 class Categoria(models.Model):
     nombre = models.CharField("Nombre", max_length=50)
@@ -17,19 +32,49 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = "Categorías"    
+
 
 class Producto(models.Model):
     nombre = models.CharField("Nombre" , max_length=50)
     precio = models.DecimalField("Precio", max_digits=9, decimal_places=2)
     estado = models.CharField("Estado", max_length=1)
     slug = models.CharField("Slug", max_length=50)
-    precio_oferta = models.DecimalField("Precio oferta", max_digits=9, decimal_places=2)
+    precio_oferta = models.DecimalField("Precio oferta", max_digits=9, decimal_places=2, null=True, blank=True)
+    porcentaje_oferta = models.DecimalField("Porcentaje descuento", max_digits=9, decimal_places=2, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
     marca_codigo = models.ForeignKey(Marca, on_delete=models.RESTRICT)
+    def __str__(self):
+       return self.nombre
+    
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = "Productos"
 
-class PResentacion(models.Model):
+class Presentacion(models.Model):
     stock = models.DecimalField("Stock Actual", max_digits=9, decimal_places=2)
     precio = models.DecimalField("Precio", max_digits=9, decimal_places=2, null=True)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     medida = models.ForeignKey(Medida, on_delete=models.CASCADE)
     color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    def __str__(self):
+       return self.producto.nombre + '' + self.medida.nombre + '' + self.colore.nombre
+
+    class Meta:
+        verbose_name = 'Presentación'
+        verbose_name_plural = "Presentaciones"   
+
+class Imagen(models.Model):
+    nombre = models.ImageField("Nombre", upload_to="productos")
+    order = models.SmallIntegerField("Orden")
+    Producto = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+
+    def __str__(self):
+       return self.nombre 
+
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = "Imagenes"
